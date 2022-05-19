8399c85776c7ab77e6aadd8f4b29dabff1dd8444
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 16:44:29 +0100

    Created Function: Game

--- main.js
+++ main.js
@@ -61,3 +61,48 @@ function PlayRound(userSelection, computerSelection) {
 
     }
 }
+
+// Function: Calls PlayRound Function and Counts Scores
+// Returns Array Format: Winner | User's Score | Computer's Score
+function Game() {
+    // Initialise Variables
+    let rounds = 5; // Holds the amount of rounds to play
+    let userScore = 0; // Will hold the score of the user
+    let computerScore = 0; // Will hold the score of the computer
+
+    // Play the Game
+    for (let i = 1; i <= rounds; i++) {
+        // Get Choices
+        let userChoice = UserPlay();
+        let computerChoice = ComputerPlay();
+
+        // Play Round
+        let winner = PlayRound(userChoice, computerChoice);
+
+        // Output Results
+        console.log(`---------------------------`);
+        console.log(`Round: ${i}`)
+        console.log(`--------`)
+        console.log(`User's Choice: ${userChoice}`);
+        console.log(`Computer's Choice: ${computerChoice}`);
+        console.log(`Winner: ${winner}`);
+
+        // Add Score
+        switch (winner) {
+            case "Computer":
+                computerScore++;
+                break;
+            case "User": 
+                userScore++;
+                break;
+        }
+    }
+
+    // Declare the Overall Winner
+    if(userScore == computerScore)
+        return ["Draw", userScore, computerScore];
+    else if (userScore > computerScore)
+        return ["User", userScore, computerScore];
+    else if (computerScore > userScore)
+        return ["Computer", userScore, computerScore];
+}
