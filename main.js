8caf69ecb26e3f8365a41c4ade35d256b707a500
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 16:15:10 +0100

    Created Function: ComputerPlay

---
+++ main.js
@@ -0,0 +1,17 @@
+// Function: Generates Either Rock/Paper/Scissors Randomly
+function ComputerPlay() {
+    // Generate Random Number
+    const number = Math.floor(Math.random() * (4 - 1) + 1);
+
+    // Set Random Number to a Choice
+    switch(number) {
+        case 1:
+            return "rock";
+        case 2:
+            return "paper";
+        case 3:
+            return "scissors";
+        default:
+            return "Error - Incorrect number generated";
+    }
+}
