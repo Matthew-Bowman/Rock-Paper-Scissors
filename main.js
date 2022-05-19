42d3f186adb5cfe05dd7a1577201b9a9be6ac090
Matthew-Bowman <matthew.bowman@tutanota.com>
2022-05-19 16:25:35 +0100

    Created Function: PlayRound

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function ComputerPlay() {
     const number = Math.floor(Math.random() * (4 - 1) + 1);
 
     // Set Random Number to a Choice
-    switch(number) {
+    switch (number) {
         case 1:
             return "rock";
         case 2:
@@ -13,5 +13,45 @@ function ComputerPlay() {
             return "scissors";
         default:
             return "Error - Incorrect number generated";
+    }
+}
+
+// Function: Takes Both Selections and Declares a Winner
+function PlayRound(userSelection, computerSelection) {
+    // Make both selections lowercase
+    userSelection = userSelection.toLocaleLowerCase();
+    computerSelection = computerSelection.toLocaleLowerCase();
+
+    // Decide the winner
+    // Check for a Draw
+    if (userSelection === computerSelection)
+        return "Draw";
+    // User Chose Rock
+    else if (userSelection === "rock") {
+        switch (computerSelection) {
+            case "paper":
+                return "Computer";
+            case "scissors":
+                return "User";
+        }
+    } 
+    // User Chose Paper
+    else if (userSelection === "paper") {
+        switch (computerSelection) {
+            case "rock":
+                return "User";
+            case "scissors":
+                return "Computer";
+        }
+    } 
+    // User Chose Scissors
+    else if (userSelection === "scissors") {
+        switch (computerSelection) {
+            case "rock":
+                return "Computer";
+            case "paper":
+                return "User";
+        }
+
     }
 }
